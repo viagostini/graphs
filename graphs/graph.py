b568f0d3b2025ebc0e5f22e62df442e1240c6fed
Vinicius Agostini <vinicius.agostini19@gmail.com>
2020-06-04 11:53:06 -0300

    added is_connected and shortest_path to graph.py

--- graphs/graph.py
+++ graphs/graph.py
@@ -3,10 +3,10 @@ Class used to represent an Undirected Graph.
 Also serves as a base class for directed graphs.
 """
 
-from collections import defaultdict
+from collections import deque, defaultdict
+
 
 class Graph:
-
     class Edge:
         """
         Internal class to represent an edge.
@@ -30,9 +30,6 @@ class Graph:
     def __len__(self):
         return len(self.nodes)
 
-    def __contains__(self, node):
-        return node in self.nodes
-
     def add_node(self, node):
         """
         Adds a new node to the graph
@@ -40,17 +37,90 @@ class Graph:
         self.nodes.add(node)
         self.adj[node] = set()
 
+    def add_multiple_nodes(self, nodes):
+        """
+        Adds multiple nodes to the graph
+        """
+        for node in nodes:
+            self.add_node(node)
+
     def add_edge(self, from_node, to_node):
         """
         Adds a new edge to the graph, also adding the nodes in case they aren't
         already in it
         """
-        if from_node not in self:
+        if from_node not in self.nodes:
             self.add_node(from_node)
-        
-        if to_node not in self:
+
+        if to_node not in self.nodes:
             self.add_node(to_node)
 
         self.adj[from_node].add(to_node)
         self.adj[to_node].add(from_node)
         self.edges.add(self.Edge(from_node, to_node))
+
+    def add_multiple_edges(self, edges):
+        """
+        Add multiple edges to the graph, adding the nodes in case they aren't
+        already in it
+        """
+        for from_node, to_node in edges:
+            self.add_edge(from_node, to_node)
+
+    def is_connected(self):
+        """
+        Returns True if the graph is connected, False otherwise
+        """
+        visited = set()
+        node = next(iter(self.nodes))
+        self.depth_first_search(node, visited=visited)
+
+        return True if len(visited) == len(self) else False
+
+    def shortest_path(self, start, end):
+        """
+        Returns one (of the possibly many) shortest paths from start node
+        to end node if one exists, None otherwise
+        """
+        parents = {}
+        distances = {start: 0}
+
+        queue = deque([start])
+        while queue:
+            node = queue.popleft()
+            for next_node in (self.adj[node] - distances.keys()):
+                parents[next_node] = node
+                distances[next_node] = distances[node] + 1
+                if next_node == end:
+                    return self._backtrace_path(start, end, parents)
+                queue.append(next_node)
+
+        return None
+
+    def _backtrace_path(self, start, end, parents):
+        """
+        Helper function to backtrace a path from parent information
+        """
+        path = deque([end])
+
+        node = end
+        while node != start:
+            path.appendleft(parents[node])
+            node = parents[node]
+
+        return list(path)
+
+    def depth_first_search(self, node=None, visited=None):
+        """
+        Performs a depth-first traversal of the graph, marking nodes that
+        have been seen in `visited`
+        """
+        if node is None:
+            node = next(iter(self.nodes))
+
+        if visited is None:
+            visited = set()
+
+        visited.add(node)
+        for next_node in (self.adj[node] - visited):
+            self.depth_first_search(next_node, visited)
--- graphs/tests/graph_test.py
+++ graphs/tests/graph_test.py
@@ -1,27 +1,72 @@
-import pytest
+from graphs.graph import Graph
 
-from graph import Graph
 
 def test_initialization():
-    g = Graph()
-    assert g.adj == {}
-    assert len(g) == 0
-    assert len(g.nodes) == 0
-    assert len(g.edges) == 0
+    graph = Graph()
+    assert graph.adj == {}
+    assert len(graph) == 0
+    assert len(graph.nodes) == 0
+    assert len(graph.edges) == 0
+
 
 def test_add_node():
-    g = Graph()
-    g.add_node(1)
-    g.add_node(2)
-    assert len(g) == 2
-    assert len(g.nodes) == 2
-    assert g.adj == {1: set(), 2: set()}
+    graph = Graph()
+    graph.add_node(1)
+    graph.add_node(2)
+    assert 1 in graph.nodes
+    assert 2 in graph.nodes
+    assert len(graph) == 2
+    assert len(graph.nodes) == 2
+    assert graph.adj == {1: set(), 2: set()}
+
+
+def test_add_multiple_nodes():
+    graph = Graph()
+    graph.add_multiple_nodes((1, 2, 3, 4, 5))
+    assert len(graph) == 5
+    assert len(graph.nodes) == 5
+    assert graph.adj == {1: set(), 2: set(), 3: set(), 4: set(), 5: set()}
+
+    graph = Graph()
+    graph.add_multiple_nodes([1, 2, 3, 4, 5])
+    assert graph.adj == {1: set(), 2: set(), 3: set(), 4: set(), 5: set()}
+
 
 def test_add_edge():
-    g = Graph()
-    g.add_node(1)
-    g.add_edge(1, 2)
-    assert len(g) == 2
-    assert len(g.nodes) == 2
-    assert len(g.edges) == 1
-    assert g.adj == {1: set([2]), 2: set([1])}
+    graph = Graph()
+    graph.add_node(1)
+    graph.add_edge(1, 2)
+    assert len(graph) == 2
+    assert len(graph.nodes) == 2
+    assert len(graph.edges) == 1
+    assert graph.adj == {1: {2}, 2: {1}}
+
+
+def test_add_multiple_edges():
+    graph = Graph()
+    graph.add_multiple_edges(((1, 2), (2, 3), (3, 4)))
+    assert len(graph) == 4
+    assert len(graph.edges) == 3
+    assert graph.adj == {1: {2}, 2: {1, 3}, 3: {2, 4}, 4: {3}}
+
+    graph = Graph()
+    graph.add_multiple_edges([(1, 2), (2, 3), (3, 4)])
+    assert graph.adj == {1: {2}, 2: {1, 3}, 3: {2, 4}, 4: {3}}
+
+
+def test_is_connected():
+    graph = Graph()
+    graph.add_multiple_edges([(1, 2), (2, 3), (3, 4), (4, 5), (2, 4)])
+    assert graph.is_connected()
+
+    graph = Graph()
+    graph.add_node(1)
+    graph.add_multiple_edges([(2, 3), (3, 4), (4, 5), (2, 4)])
+    assert not graph.is_connected()
+
+
+def test_shortest_path():
+    graph = Graph()
+    graph.add_multiple_edges([(1, 2), (2, 3), (3, 4), (4, 5), (2, 4)])
+    assert graph.shortest_path(1, 4) == [1, 2, 4]
+    assert graph.shortest_path(1, 6) is None
